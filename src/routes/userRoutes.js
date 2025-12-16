a52e0974740eb449ab1c62a5535592a7265664b0
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-12-16 10:30:24 +0600

    updated user role

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const { upsertUser, getAllUsers, getUserRole } = require('../controllers/userController');
+const { upsertUser, getAllUsers, getUserRole, updateUserRole, deleteUser } = require('../controllers/userController');
+const { verifyToken, verifyAdmin } = require('../middlewares/authMiddleware'); 
 
 const router = express.Router();
 
 router.post('/users', upsertUser); 
+router.get('/users/role/:email', verifyToken, getUserRole); 
 
-router.get('/users', getAllUsers); 
+router.get('/users', verifyToken, verifyAdmin, getAllUsers);
 
-router.get('/users/role/:email', getUserRole); 
+router.patch('/users/role/:id', verifyToken, verifyAdmin, updateUserRole);
+
+router.delete('/users/:id', verifyToken, verifyAdmin, deleteUser);
 
 module.exports = router;
