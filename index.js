305f48628dbe16f7a30cd177fc64334ea9baf8d2
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-12-18 23:13:26 +0600

    added updated link

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://etuition-bd.web.app',
-    'https://venerable-quokka-b8c38c.netlify.app'
+    'https://tuition-client-five.vercel.app/',
+    'https://fascinating-pavlova-390b2e.netlify.app/'
   ],
   credentials: true
 }));
